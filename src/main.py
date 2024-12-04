8995d4bb89cf0135b3a01edad000a072f5e1792f
s19835 <s19835@sci.pdn.ac.lk>
2024-12-04 09:34:43 +0530

    Add: Tasks for every agents

--- src/main.py
+++ src/main.py
@@ -43,5 +43,24 @@ def main():
         tools=[]
     )
 
+    # create tasks for the agents
+
+    # task1: conduct comprehensive analysis on the AI topic
+    task1 = Task(
+        description="""Conduct a comprehensive analysis of the latest advancements in AI in 2024.
+        Identify key trends, breakthrough technologies, and potential industry impacts.
+        Your final answer MUST be a full analysis report""",
+        agent=researcher
+    )
+
+    # task2: using provided insight develp a well structured blog post
+    task2 = Task(
+        description="""Using the insights provided, develop an engaging blog post that highlights the most significant AI advancements.
+        Your post should be informative yet accessible, catering to a tech-savvy audience.
+        Make it sound cool, avoid complex words so it doesn't sound like AI.
+        Your final answer MUST be the full blog post of at least 4 paragraphs and MUST contain MORE THAN 100 WORDS.""",
+        agent=writer
+    )
+
 if __name__ == "__main__":
     main()
