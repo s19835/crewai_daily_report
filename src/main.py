4acef097ec713f449b0b12245703a27ed0e83b77
s19835 <s19835@sci.pdn.ac.lk>
2024-12-04 09:04:56 +0530

    Create: model

--- src/main.py
+++ src/main.py
@@ -2,8 +2,15 @@ import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from crewai import Agent, Task, Process, Crew
 
-# get the gemini api key from env
-api_key = os.getenv('GEMINI_API_KEY')
-if api_key is None:
-    raise ValueError("Api key is not set in the env variable!")
+def main():
+    # get the gemini api key from env
+    api_key = os.getenv('GEMINI_API_KEY')
+    if api_key is None:
+        raise ValueError("Api key is not set in the env variable!")
 
+    # create llm using gemini pro
+    llm = ChatGoogleGenerativeAI(model="gemini-pro", verbose=True, temperature=0.6, api_key=api_key)
+    
+
+if __name__ == "__main__":
+    main()
