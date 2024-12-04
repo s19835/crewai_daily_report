d65feec9c43d7a35a020a9c636b36e9fe459ff08
s19835 <s19835@sci.pdn.ac.lk>
2024-12-04 08:58:23 +0530

    manage:API key

--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from crewai import Agent, Task, Process, Crew
+
+# get the gemini api key from env
+api_key = os.getenv('GEMINI_API_KEY')
+if api_key is None:
+    raise ValueError("Api key is not set in the env variable!")
+
